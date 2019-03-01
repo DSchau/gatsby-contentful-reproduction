0c3845a80f9ffdae01233c9c5fc19d2b41be1343
Dustin Schau <dustinschau@gmail.com>
2019-03-01 09:37:14 -0600

    chore: readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,8 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
